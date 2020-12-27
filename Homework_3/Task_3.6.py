6520d74ae823dec04da8bc4c6adcbc1232882eb8
nikkav79 <nikolaykapustnikov@gmail.com>
2020-12-27 14:19:45 +0300

    add Task_3.6

---
+++ Homework_3/Task_3.6.py
@@ -0,0 +1,8 @@
+lowercase_word = input('Enter the words in lowercase: ')
+
+def increase_one_word(words):
+    uppercase_word = words.title()
+    print(uppercase_word)
+
+increase_one_word(lowercase_word)
+
